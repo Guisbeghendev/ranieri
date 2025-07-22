b72542acb04b1104ca51d3299376cca84b8794a5
Guisbeghendev <guisbeghendev@gmail.com>
2025-07-21 21:31:41 -0300

    atualização

--- ranieri_project/wsgi.py
+++ ranieri_project/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+# Importe load_dotenv para carregar o arquivo .env
+from dotenv import load_dotenv # ADICIONE ESTA LINHA
+
+# Define o caminho para o arquivo .env na raiz do projeto
+# Isso garante que o .env seja carregado antes que o Django tente acessar as variáveis
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path) # Carrega as variáveis do .env
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranieri_project.settings')
 
