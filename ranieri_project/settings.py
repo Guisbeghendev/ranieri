ce1400fd1590429103a0f6c7184bff6d4e316aff
Guisbeghendev <guisbeghendev@gmail.com>
2025-07-25 22:48:49 -0300

    atualização

--- ranieri_project/settings.py
+++ ranieri_project/settings.py
@@ -57,7 +57,8 @@ AUTHENTICATION_BACKENDS = (
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.common.CommonMiddleware',
+    #'django.contrib.common.CommonMiddleware',#talvez esta errado
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
