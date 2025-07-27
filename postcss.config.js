0c71f821a45ff810ff175c1b01330706b4f7c245
Guisbeghendev <guisbeghendev@gmail.com>
2025-07-27 13:04:24 -0300

    atualização

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 // postcss.config.js
 // Este arquivo configura o PostCSS para usar os plugins necessários.
-module.exports = { // Adaptado de 'export default' para Node.js
+module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {}, // Esta é a linha CRÍTICA para a correção do erro
+    tailwindcss: {}, // CORREÇÃO: Usar 'tailwindcss' diretamente para v4.x.x
     autoprefixer: {}, // Habilita o Autoprefixer
   },
 };
