8602c3f5e16f464be70cc434157c3add108694f5
Guisbeghendev <guisbeghendev@gmail.com>
2025-07-31 18:14:33 -0300

    atualização staging

--- galleries_pub/urls.py
+++ galleries_pub/urls.py
@@ -1,14 +1,12 @@
-# galleries_pub/urls.py
 from django.urls import path
 from . import views
 
-app_name = 'galleries_pub' # Define o namespace da URL para este app
+app_name = 'galleries_pub'
 
 urlpatterns = [
     # Rota para listar todas as galerias públicas
     path('', views.PublicGalleryListView.as_view(), name='public_gallery_list'),
     # Rota para ver os detalhes de uma galeria pública específica
     path('<int:pk>/', views.PublicGalleryDetailView.as_view(), name='public_gallery_detail'),
-    # NOVO: Rota para curtir/descurtir uma galeria pública (via AJAX)
-    path('<int:pk>/like/', views.like_public_gallery, name='like_public_gallery'),
+    # A rota para o like foi removida.
 ]
