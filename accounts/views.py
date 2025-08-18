c95e0e36c24690e095872a4faec338eb1498d53d
Guisbeghendev <guisbeghendev@gmail.com>
2025-08-18 18:16:33 -0300

    atualização staging

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm, UserLoginForm, UserForm, ProfileForm
 from django.contrib.auth import login, logout, get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView, View, DeleteView
 from django.urls import reverse_lazy
 from django.contrib import messages
