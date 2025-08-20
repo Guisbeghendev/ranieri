8b01bfde47f37eb4138fbd886499c7e4360eb4fe
Guisbeghendev <guisbeghendev@gmail.com>
2025-08-20 19:08:57 -0300

    atualização staging

--- core/apps.py
+++ core/apps.py
@@ -2,9 +2,16 @@
 
 from django.apps import AppConfig
 
+
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    verbose_name = 'Core'
 
     def ready(self):
-        import core.signals # Importa o módulo de signals para que os signals sejam registrados
+        # Importa o módulo de signals para que os signals sejam registrados
+        import core.signals
+
+        # Importa o módulo de administração para que o Django o descubra
+        # Esta linha garante que a classe de administração para Repertorio_Coral seja registrada.
+        import core.admin
