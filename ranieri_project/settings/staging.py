2024afaee641d76f9a8e4fe9af3d932884a631b8
Guisbeghendev <guisbeghendev@gmail.com>
2025-09-04 10:37:16 -0300

    atualização

--- ranieri_project/settings/staging.py
+++ ranieri_project/settings/staging.py
@@ -1,33 +1,28 @@
 import os
-# Importa todas as configurações do seu arquivo settings.py principal
-from ranieri_project.settings.base import * # Acessa o arquivo base.py corretamente
+from pathlib import Path
+from ranieri_project.settings.base import *
 
 # --- Configurações Específicas para Staging ---
 
-# SECRET_KEY para segurança da aplicação.
-# Adicionamos a lógica de leitura da variável de ambiente aqui,
-# garantindo que o Django possa ler a chave do Apache.
 SECRET_KEY = os.environ.get('SECRET_KEY', 'sua_chave_de_desenvolvimento_aqui')
 
 DEBUG = True
 
-# A lista de hosts permitidos precisa incluir o nome de domínio que
-# está sendo usado, 'staging.escolajoseranieri.com.br'.
 ALLOWED_HOSTS = ['staging.escolajoseranieri.com.br', 'localhost', '127.0.0.1']
 
-# Configuração do banco de dados para o ambiente de testes (MySQL)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Motor do banco de dados
-        'NAME': 'ranieri_staging_db',        # Nome do banco de dados de testes
-        'USER': 'ranieri_staging_user',      # Usuário do banco de dados de testes
-        'PASSWORD': 'Gsp@root2025',          # Senha do usuário de testes
-        'HOST': 'localhost',                 # Host do banco de dados
-        'PORT': '',                          # Porta do MySQL (vazio para padrão)
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ranieri_staging_db',
+        'USER': 'ranieri_staging_user',
+        'PASSWORD': 'Gsp@root2025',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
-# Caminho para os arquivos de mídia (imagens, uploads, etc)
-# Apontamos para o diretório "mediafiles" que você já possui.
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/mediafiles/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
