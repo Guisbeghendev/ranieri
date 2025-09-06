0afcd7cb88d79e8b1e63184b0d9413d2bf0384dd
Guisbeghendev <guisbeghendev@gmail.com>
2025-09-06 13:47:30 -0300

    atualização

--- simcozinha/views.py
+++ simcozinha/views.py
@@ -3,7 +3,7 @@ from core.models import SimCo_Receita
 
 class SimCozinhaPageView(ListView):
     model = SimCo_Receita
-    template_name = 'simoninha_page.html'
+    template_name = 'simcozinha_page.html'
     context_object_name = 'receitas'
     paginate_by = 10
     ordering = ['-inclusion_date']
