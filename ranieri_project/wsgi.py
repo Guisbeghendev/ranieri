5a4111a255c6c23c2586221a16373914d7ebcca9
Guisbeghendev <guisbeghendev@gmail.com>
2025-09-07 11:53:32 -0300

    atualização

--- ranieri_project/wsgi.py
+++ ranieri_project/wsgi.py
@@ -1,17 +1,25 @@
-
-"""
-WSGI config for ranieri_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranieri_project.settings')
+# Carrega as variáveis de ambiente a partir do arquivo .env.
+# Isso deve ser a primeira coisa a ser feita.
+# A função irá procurar o arquivo na pasta onde o wsgi.py está.
+load_dotenv()
 
+# Define o caminho base do projeto, que é o diretório que contém este arquivo.
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+# Adiciona o diretório raiz do projeto ao PATH do Python.
+sys.path.insert(0, str(PROJECT_ROOT))
+
+# Tenta carregar a configuração do ambiente usando uma variável de sistema.
+# Se a variável 'DJANGO_SETTINGS_MODULE' não estiver definida,
+# ele irá usar 'local.py' por padrão.
+settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'ranieri_project.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
+# Agora sim, carrega a aplicação WSGI do Django.
 from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
